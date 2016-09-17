ab2326cf33ab856814b556bd2643c7694e791db7
siliconion <chihchiayang@gmail.com>
2016-09-17 17:04:30 -0500

    remove artwork from widget and move up

--- client/components/Room.js
+++ client/components/Room.js
@@ -7,21 +7,21 @@ export default class Room extends React.Component {
     super(props);
     this.state = {
       track: 'https://soundcloud.com/logic_official/flexicution-1?in=hennessy/sets/never-stop-never-settle',
-      users: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
+      users: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
       djs: [],
     };
     this.widget = null;
   }
 
-
   componentDidMount() {
     load('https://w.soundcloud.com/player/api.js', () => {
       this.widget = window.SC.Widget('soundcloudPlayer'); // eslint-disable-line new-cap
-      this.widget.load(this.state.track);
+      this.widget.show_artwork = false;
+      this.widget.load(this.state.track, { show_artwork: false });
     });
 
     const djs = [{ 'id': 3203, 'username': 'manyoora', 'avatar_url': 'https://a1.sndcdn.com/images/default_avatar_large.png' },
-    { 'id': 4973508, 'username': 'Macabre!', 'avatar_url': "https://i1.sndcdn.com/avatars-000218947088-qgg05p-large.jpg" },
+    { 'id': 4973508, 'username': 'Macabre!', 'avatar_url': 'https://i1.sndcdn.com/avatars-000218947088-qgg05p-large.jpg' },
     { 'id': 9509, 'username': 'compositeone', 'avatar_url': 'https://i1.sndcdn.com/avatars-000000607500-271hqp-large.jpg' },
     {}];
     this.setState({
@@ -30,9 +30,6 @@ export default class Room extends React.Component {
   }
 
   render() {
-    // <Stage />
-    // <Vote />
-    // <Crowd />
     return (
       <div className="room">
         <div className="container">
@@ -51,8 +48,7 @@ export default class Room extends React.Component {
                   );
                 } else {
                   return (
-                    <div className="stage--seat empty" key={dj.id}>
-                    </div>
+                    <div className="stage--seat empty" key={dj.id} />
                   );
                 }
               })
@@ -60,7 +56,7 @@ export default class Room extends React.Component {
           </div>
 
           <div id="vote" className="vote row">
-            <div className="col-xs-4"></div>
+            <div className="col-xs-4" />
             <div className="vote--btns col-xs-4">
               <button className="btn btn-success">Upvote</button>
               <button className="btn btn-danger">Downvote</button>
@@ -68,20 +64,17 @@ export default class Room extends React.Component {
             <div className="vote--djQueue col-xs-4">
               <button className="btn btn-default">Queue for DJ</button>
             </div>
-              
           </div>
-
+          <iframe id="soundcloudPlayer" width="100%" height="100" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=" />
           <div className="crowd">
             {
               this.state.users.map((user) => {
                 return (
-                  <div className="crowd--user"></div>
+                  <div className="crowd--user" />
                 );
               })
             }
           </div>
-
-          <iframe id="soundcloudPlayer" width="100%" height="166" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=" />
         </div>
       </div>
     );
