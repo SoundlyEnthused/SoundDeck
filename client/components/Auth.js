b9d84458ae3bc104624591b9a2303879bb28a8cc
Christopher Kiel <no-fi@users.noreply.github.com>
2016-09-17 14:42:38 -0500

    Lints Auth.js in order to remove all errors

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -4,18 +4,18 @@ const clientId = process.env.CLIENT_ID;
 const login = {};
 
 
-login.signin = function (){
+login.signin = () => {
   SC.initialize({
     client_id: clientId,
     redirect_uri: 'http://localhost:4000/callback.html',
   });
 
-  return SC.connect().then(function () {
-    return SC.get('/me');
-  }).then(function(me) {
-    console.log("result of me", me);
-    return me;
-  });
+  return SC.connect()
+    .then(() => SC.get('/me'))
+    .then((me) => {
+      console.log('result of me: ', me);
+      return me;
+    });
 };
 
 module.exports = login;
