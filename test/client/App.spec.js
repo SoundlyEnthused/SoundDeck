389368276fd5cdcb28ab9231dce699cc46655d94
Robert Ferguson <rob@rewferguson.com>
2016-09-16 19:44:59 -0500

    add tests for App Lobby and Room

--- test/client/App.spec.js
+++ test/client/App.spec.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
-import MyComponent from '../../client/components/App';
+import App from '../../client/components/App';
+import $ from 'jquery';
 
 describe('<App />', () => {
 
   it('renders a nav bar', () => {
-    const wrapper = mount(<MyComponent />);
+    const wrapper = mount(<App />);
     expect(wrapper.find('nav').length).to.equal(1);
   });
 
+  it('renders the lobby if there is no current room', () => {
+    const wrapper = mount(<App />);
+    wrapper.setState({ currentRoom: undefined });
+    expect(wrapper.find('#lobby').hasClass('active')).to.equal(true);
+  });
+
+});
+
+describe('<Lobby>', () => {
+  it('selects a room when the join button is pressed', () => {
+    const wrapper = mount(<App />);
+    wrapper.find('.joinBtn').first().simulate('click');
+    expect(wrapper.state('currentRoom')).to.not.equal(undefined);
+  });
+
+  it('renders a room to the page when the join button is pressed', () => {
+    const wrapper = mount(<App />);
+    wrapper.find('.joinBtn').first().simulate('click');
+    expect(wrapper.find('.room').length).to.equal(1);
+  });
 });
