4386c92debbb744f0c01779e59a6991a3d8486ab
siliconion <chihchiayang@gmail.com>
2016-09-23 11:19:14 -0500

    refactor so currentRoom state is roomId, not room object

--- test/client/components/App.spec.js
+++ test/client/components/App.spec.js
@@ -4,69 +4,53 @@ import { mount } from 'enzyme';
 import App from '../../../client/components/App';
 import $ from 'jquery';
 
-// describe() is a strictly visual function => it formats output to 
+// describe() is a strictly visual function => it formats output to
 // be more readable; can organize tests with different describe() functions
 
 describe('<App>', () => {
-  it('renders a nav bar', () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find('nav').length).to.equal(1);
+  describe('initial rendering', () => {
+    it('renders a nav bar', () => {
+      const wrapper = mount(<App />);
+      expect(wrapper.find('nav').length).to.equal(1);
+    });
+
+    it('renders the lobby if there is no current room', () => {
+      const wrapper = mount(<App />);
+      wrapper.setState({ currentRoom: undefined });
+      expect(wrapper.find('#lobby').hasClass('active')).to.equal(true);
+    });
   });
 
-  it('renders the lobby if there is no current room', () => {
-    const wrapper = mount(<App />);
-    wrapper.setState({ currentRoom: undefined });
-    expect(wrapper.find('#lobby').hasClass('active')).to.equal(true);
-  });
-});
+  describe('App functions', () => {
+    describe('joinRoom', () => {
+      const app = new App();
+      it('should be a function', () => {
+        expect(app.joinRoom).to.be.a('function');
+      });
 
-describe('joinRoom', () => {
-  const app = new App();
-  it('should be a function', () => {
-    expect(app.joinRoom).to.be.a('function');
-  });
+      it('should have props for the current room', () => {
+        const wrapper = mount(<App />);
+        expect(wrapper.props().currentRoom).to.be.defined;
+      });
+    });
 
-  it('should have props for the current room', () => {
-    const wrapper = mount (<App />);
-    expect(wrapper.props().currentRoom).to.be.defined;
-  });
-});
+    describe('loggingIn', () => {
+      const app = new App();
+      it('should be a function', () => {
+        expect(app.joinRoom).to.be.a('function');
+      });
 
-describe('loggingIn', () => {
-  const app = new App();
-  it('should be a function', () => {
-    expect(app.joinRoom).to.be.a('function');
-  });
-
-  it('should have props for the current user data', () => {
-    const wrapper = mount (<App />);
-    expect(wrapper.props().userData).to.be.defined;
-  });
-});
-
-describe('<Lobby>', () => {
-  it('selects a room when the join button is pressed', () => {
-    const wrapper = mount(<App />);
-    wrapper.find('.joinBtn').first().simulate('click');
-    expect(wrapper.state('currentRoom')).to.not.equal(undefined);
-  });
-/*
-  it('renders a room to the page when the join button is pressed', () => {
-    const wrapper = mount(<App />);
-    wrapper.find('.joinBtn').first().simulate('click');
-    expect(wrapper.find('.room').length).to.equal(1);
-  });
-  */
-});
-
-describe('<Navigation Bar>', () => {
-  it ('displays a login button', () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find('#LoginButton')).to.exist;
-  });
-
-  it ('displays a playlist button', () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find('#PlaylistButton')).to.exist;
-  });
+      it('should have props for the current user data', () => {
+        const wrapper = mount(<App />);
+        expect(wrapper.props().userData).to.be.defined;
+      });
+    });
+  })
+  /*
+    it('renders a room to the page when the join button is pressed', () => {
+      const wrapper = mount(<App />);
+      wrapper.find('.joinBtn').first().simulate('click');
+      expect(wrapper.find('.room').length).to.equal(1);
+    });
+    */
 });
