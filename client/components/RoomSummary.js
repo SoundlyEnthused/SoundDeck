4d5626ff7ec681e1a5f35d913c11b22e8c829c4c
Christopher Kiel <no-fi@users.noreply.github.com>
2016-10-05 11:00:38 -0500

    Refactors RoomSummary to be a pure function component

--- client/components/RoomSummary.js
+++ client/components/RoomSummary.js
@@ -1,29 +1,25 @@
 import React from 'react';
 
-export default class RoomSummary extends React.Component {
-  render() {
-    return (
-      <li className="row lobby--room list-unstyled">
-        <div className="col-xs-6">
-          <span className="roomname">{this.props.name}</span>
-        </div>
-        <div className="col-xs-6 lobby--joinBtn" >
-          {
-            this.props.djs.map((dj, idx) =>
-              (dj !== null
-              ? <div className="active-dj-slot" key={idx}><img src={dj.avatar_url} /></div>
-              : <div className="empty-dj-slot" key={idx} />)
-            )
-          }
-          <span className="roomcount">{this.props.count}</span>
-          <button className="btn btn-primary joinBtn" onClick={this.props.onJoinClick}>
-          Join
-          </button>
-        </div>
-      </li>
-    );
-  }
-}
+const RoomSummary = props => (
+  <li className="row lobby--room list-unstyled">
+    <div className="col-xs-6">
+      <span className="roomname">{props.name}</span>
+    </div>
+    <div className="col-xs-6 lobby--joinBtn" >
+      {
+        props.djs.map((dj, idx) =>
+          (dj !== null
+          ? <div className="active-dj-slot" key={idx}><img src={dj.avatar_url} alt="active dj" /></div>
+          : <div className="empty-dj-slot" key={idx} />)
+        )
+      }
+      <span className="roomcount">{props.count}</span>
+      <button className="btn btn-primary joinBtn" onClick={props.onJoinClick}>
+      Join
+      </button>
+    </div>
+  </li>
+);
 
 RoomSummary.propTypes = {
   name: React.PropTypes.string,
@@ -31,3 +27,5 @@ RoomSummary.propTypes = {
   djs: React.PropTypes.array,
   onJoinClick: React.PropTypes.func,
 };
+
+export default RoomSummary;
