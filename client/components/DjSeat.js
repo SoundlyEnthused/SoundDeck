9276e30d08af8ed06497f6c3d9005c7d84e1c2fd
Christopher Kiel <no-fi@users.noreply.github.com>
2016-10-06 11:49:40 -0500

    Adds strikes prop to DJ in components

--- client/components/DjSeat.js
+++ client/components/DjSeat.js
@@ -12,6 +12,7 @@ const DjSeat = props => (
         data-animation="true"
         data-toggle="tooltip"
         data-likes={props.likes || 0}
+        data-strikes={props.strikes}
       >
         <img src={props.avatar_url} alt={props.username} />
       </div>
@@ -23,6 +24,7 @@ DjSeat.propTypes = {
   avatar_url: React.PropTypes.string,
   username: React.PropTypes.string,
   likes: React.PropTypes.number,
+  strikes: React.PropTypes.number,
 };
 
 export default DjSeat;
