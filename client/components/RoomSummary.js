7d115d63dc9f481bbe7e17019f0999aebbc74a57
Aaron Ventura <aaronjosephventura@gmail.com>
2016-10-07 11:30:30 -0500

    minor UX tweaks

--- client/components/RoomSummary.js
+++ client/components/RoomSummary.js
@@ -3,21 +3,21 @@ import React from 'react';
 const RoomSummary = props => (
   <li className="lobby--room list-unstyled">
     <div className="lobby--roomWrapper">
-    <div className="lobby--roomname">{props.name}</div>
+      <div className="lobby--roomname">{props.name}</div>
 
-    <div className="lobby--join" >
-      {
+      <div className="lobby--join" >
+        {
         props.djs.map((dj, idx) =>
           (dj !== null
           ? <div className="active-dj-slot" key={idx}><img src={dj.avatar_url} alt="active dj" /></div>
           : <div className="empty-dj-slot" key={idx} />)
         )
       }
-      <span className="roomcount">{props.count}</span>
-      <button className="btn btn-primary joinBtn" onClick={props.onJoinClick}>
+        <span className="roomcount">{props.count}</span>
+        <button className="btn btn-primary joinBtn" onClick={props.onJoinClick}>
         Join
-      </button>
-    </div>
+        </button>
+      </div>
     </div>
   </li>
 );
